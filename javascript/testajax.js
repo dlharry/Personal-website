a34321ef4ce82620258627332cc9063110918bc8
dlharry <dlharry@126.com>
2022-02-24 13:37:07 +0000

    update script in html, dunno what to do

--- javascript/testajax.js
+++
@@ -1,8 +0,0 @@
-$.post("ajax/test.html", function (data) {
-    $(".result").html(data);
-});
-function sendrequest() {
-    res = document.getElementById('output_result');
-    var url = '../php/testajax.php';
-    jQuery.post(url, data, success(data, textStatus, jqXHR), dataType);
-}
